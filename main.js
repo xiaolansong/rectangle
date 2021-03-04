381251e1a7e9465e0bb0d8825c0b64da6b0b1292
lubing <907499926@qq.com>
2021-03-04 10:22:44 +0800

    upload

---
+++ main.js
@@ -0,0 +1,20 @@
+$(()=>{
+  //get dom elem
+  let $width = $('#width'),
+      $height = $('#height'),
+      $btnCal = $('#calc')
+      $perimeter = $('#perimeter'),
+      $area = $('#area');
+  /*calc button click*/
+$btnCal.click(()=>{
+  //get value
+  let w = Number($width.val()),
+      h = Number($height.val());
+  //calc
+  let p = Number((w+h)*2),
+      a = Number(w*h);
+  //output
+  $perimeter.val(p);
+  $area.val(a);
+});
+})
